7d3e0ed46f367a924a694180e9c34202b392dc08
GReaper <xavanakhet13@gmail.com>
2018-11-01 21:15:38 +0700

    fix convert id string to object id

--- api/login/index.js
+++ api/login/index.js
@@ -3,20 +3,4 @@ module.exports = (router, lib) => {
     const middlerware = require('./middlerware')(lib);
 
     router.post('/login', controller.login);
-    // router.get("/testlogin", (req, res, next) => {
-    //     if (req.isAuthenticated()) {
-    //         delete user.username;
-    //         delete user.password;
-    //         res.status(200).json({
-    //             code: 200,
-    //             message: "OK",
-    //             data: req.user
-    //         })
-    //     } else {
-    //         res.status(800).json({
-    //             code: 300,
-    //             message: "Not access"
-    //         });
-    //     }
-    // }, (req, res) => {});
 };
