327e3927d05d661b541e86d12c0048c6dcae5c5b
GReaper <xavanakhet13@gmail.com>
2018-11-01 15:13:58 +0700

    add middleware login

--- api/record/index.js
+++ api/record/index.js
@@ -1,11 +1,12 @@
 module.exports = (router, lib) => {
-  const controller = require('./controller')(lib);
+    const controller = require('./controller')(lib);
+    const loginMiddleware = require('./../login/middlerware');
 
-  router.get('/record', controller.list);
+    router.get('/record', controller.list);
 
-  router.post('/record', controller.create);
+    router.post('/record', controller.create);
 
-  router.delete('/record/:id', controller.remove);
+    router.delete('/record/:id', controller.remove);
 
-  router.put('/record/:id', controller.update);
+    router.put('/record/:id', controller.update);
 };
